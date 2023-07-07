8a9b887e31e6b8f9dbf8742e3f5b1cb63ec364ff
Surya Krishna Moorthy <37917817+SuryaKrishnaMoorthy@users.noreply.github.com>
2023-07-07 11:02:56 -0400

    add documentation of endpoints

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express"),
   morgan = require("morgan"),
   fs = require("fs"),
-  path = require("path");
+  path = require("path"),
+  { v4: uuidv4 } = require("uuid");
 
 const topMovies = require("./data/movies.json");
+let users = require("./data/users.json");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(morgan("common"));
 
 app.use(express.static("public"));
@@ -15,8 +19,137 @@ app.get("/", (req, res) => {
   res.send("Welcome to movies_API");
 });
 
+//GET all movies
 app.get("/movies", (req, res) => {
-  res.send(topMovies);
+  res.status(200).send(topMovies);
+});
+
+//GET a single movie
+app.get("/movies/:movieTitle", (req, res) => {
+  const movie = topMovies.find((mov) => mov.title === req.params.movieTitle);
+
+  if (movie) {
+    res.status(200).send(movie);
+  } else {
+    res.status(404).send("Movie cant be found!");
+  }
+});
+
+//GET genre by name
+app.get("/movies/genre/:genreName", (req, res) => {
+  const { genreName } = req.params;
+  const genre = topMovies.find(
+    (mov) => mov.genre.name.toLowerCase === genreName.toLowerCase
+  );
+
+  if (genre) {
+    res.status(200).send(genre.genre);
+  } else {
+    res.status(404).send("Genre cant be found!");
+  }
+});
+
+//GET director by name
+app.get("/movies/director/:directorName", (req, res) => {
+  const { directorName } = req.params;
+  const movie = topMovies.find(
+    (mov) => mov.director.name.toLowerCase() === directorName.toLowerCase()
+  );
+
+  if (movie) {
+    res.status(200).send(movie.director);
+  } else {
+    res.status(404).send("Director cant be found!");
+  }
+});
+
+app.get("/users", (req, res) => {
+  res.status(200).send(users);
+});
+
+app.post("/users", (req, res) => {
+  if (!req.body.name) {
+    res.status(400).send("User details required");
+  } else {
+    const newUser = {
+      id: uuidv4(),
+      name: req.body.name,
+      favoriteMovies: req.body.favoriteMovies || [],
+    };
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+
+//Allow users to update their user info (username)
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find((user) => user.id == id);
+
+  if (!user) {
+    res.status(400).send("User not found");
+  } else {
+    user.name = updatedUser.name;
+    res.status(200).send(user);
+  }
+});
+
+//Allow users to add a movie to their list of favorites
+app.put("/users/:id/:movieId", (req, res) => {
+  const { id, movieId } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find((user) => user.id === id);
+  let movie = topMovies.find((movie) => movie.id === movieId);
+
+  if (!user) {
+    res.status(400).send("User not found");
+  } else if (!movie) {
+    res.status(400).send("Movie not found");
+  } else {
+    user.favoriteMovies.push(movie.title);
+    res
+      .status(200)
+      .send(`${movie.title} has been added to user ${id}'s favorites`);
+  }
+});
+
+//Allow users to remove a movie from their list of favorites
+app.delete("/users/:id/:movieId", (req, res) => {
+  const { id, movieId } = req.params;
+
+  let user = users.find((user) => user.id === id);
+  let movie = topMovies.find((movie) => movie.id === movieId);
+
+  if (!user) {
+    res.status(400).send("User not found");
+  } else if (!movie) {
+    res.status(400).send("Movie not found");
+  } else {
+    user.favoriteMovies = user.favoriteMovies.filter(
+      (item) => item !== movie.title
+    );
+
+    res
+      .status(200)
+      .send(`${movie.title} has been removed from user ${id}'s favorites`);
+  }
+});
+
+//Allow existing users to deregister
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find((user) => user.id === id);
+
+  if (!user) {
+    res.status(400).send("User not found");
+  } else {
+    users = users.filter((user) => user.id !== id);
+    res.status(200).send(`User ${id} has been deregistered`);
+  }
 });
 
 app.use((err, req, res, next) => {
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Movie_API
+
+## Requirements
+
+Return a list of ALL movies to the user;
+Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by title to the user;
+Return data about a genre (description) by name/title (e.g., “Thriller”);
+Return data about a director (bio, birth year, death year) by name;
+Allow new users to register;
+Allow users to update their user info (username);
+Allow users to add a movie to their list of favorites (showing only a text that a movie has been added—more on this later);
+Allow users to remove a movie from their list of favorites (showing only a text that a movie has been removed—more on this later);
+Allow existing users to deregister (showing only a text that a user email has been removed—more on this later).
